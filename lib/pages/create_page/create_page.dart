bb2af7afd96b5fdeaf50a9eacd026d88d539edc6
recmedinapachas21 <recmedinapachas21@gmail.com>
2024-07-09 22:16:08 +0000

    Prueba2

--- lib/pages/create_page/create_page.dart
+++ lib/pages/create_page/create_page.dart
@@ -1,2 +1,12 @@
 
-//prueba 1
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2941620892.
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
