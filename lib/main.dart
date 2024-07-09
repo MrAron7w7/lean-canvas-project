1e89fd86c0e5cc409d26e0d7c9c034ed375925a2
AronDev <magallanestorresaron@gmail.com>
2024-07-09 16:40:32 -0500

    ediando splash y main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lean_canvas/pages/splash_page/splash_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-            //child: Text('Hello World flutter'),
-            ),
-      ),
+    return const MaterialApp(
+      title: 'Lean Canvas',
+      home: SplashPage(),
     );
   }
 }
