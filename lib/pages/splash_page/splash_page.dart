3a43285ebb931d211ac7d1c5e32427e00a805438
AronDev <magallanestorresaron@gmail.com>
2024-07-10 15:15:18 -0500

    Añadiendo splash pages con el login

--- lib/pages/splash_page/splash_page.dart
+++ lib/pages/splash_page/splash_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
