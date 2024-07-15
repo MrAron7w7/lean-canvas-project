57eb7a4ac406e9aceba05b37b2ebd20a57e9ced0
AronDev <magallanestorresaron@gmail.com>
2024-07-15 17:34:24 -0500

    Actualizando valores

--- lib/pages/splash_page/splash_page.dart
+++ lib/pages/splash_page/splash_page.dart
@@ -10,23 +10,6 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-
-import 'package:flutter/material.dart';
-
-class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
-
-  @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
-  Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xff1B264F),
       body: Center(
